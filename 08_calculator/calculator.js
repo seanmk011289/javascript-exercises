60687328b6e4e045931c3e00395d244a75d0f71b
Sean Kennedy <seanmk011289@gmail.com>
2024-04-13 21:41:44 -0500

    calc and palindrome exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function (x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function (x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((x, y) => {
+    return x + y;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((x, y) => {
+    return x * y;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function (x, y) {
+  let arr = [];
+  for (var i = 0; i < y; i++) {
+    arr = [...arr, x];
+  }
+  return arr.reduce((a, b) => {
+    return a * b;
+  }, 1);
 };
 
-const factorial = function() {
-	
+const factorial = function (x) {
+  let arr = [];
+  for (var i = x; i > 0; i--) {
+    arr = [...arr, x];
+    x = x - 1;
+  }
+  console.log(arr);
+  return arr.reduce((a, b) => {
+    return a * b;
+  }, 1);
 };
 
 // Do not edit below this line
@@ -29,5 +47,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (x) {
+  var p1 = x.replaceAll(" ", "");
+  p1 = p1.replace(/[.,\/#!?$%\^&\*;:{}=\-_`~()]/g, "").toLowerCase();
+  p1 = p1.split("").join("");
+  var p2 = p1.split("").reverse().join("");
+  if (p2 == p1) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
