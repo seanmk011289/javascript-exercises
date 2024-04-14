70a2aface0adc491b6eb35246b86355ff7f860cf
Sean Kennedy <seanmk011289@gmail.com>
2024-04-14 10:08:59 -0500

    adding find the oldest code

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+  let year = new Date().getFullYear();
+  let filterWithYear = arr
+    .filter((x) => x.yearOfDeath)
+    .map((x) => {
+      return { ...x, age: x.yearOfDeath - x.yearOfBirth };
+    });
+  let filterWithoutYear = arr
+    .filter((x) => !x.yearOfDeath)
+    .map((x) => {
+      return { ...x, age: year - x.yearOfBirth };
+    });
+  let combined = [...filterWithYear, ...filterWithoutYear];
+  console.log(combined);
+  return combined.sort((x, y) => y.age - x.age)[0];
 };
 
 // Do not edit below this line
