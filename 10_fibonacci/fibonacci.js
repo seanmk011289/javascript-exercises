4b07b4b020091fcd9fc1d76610ddb8cb7c4a1585
Sean Kennedy <seanmk011289@gmail.com>
2024-04-13 22:55:31 -0500

    fibo code

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,30 @@
-const fibonacci = function() {
-
+const fibonacci = function (numb) {
+  let num = parseInt(numb);
+  if (num < 0) {
+    return "OOPS";
+  }
+  if (num == 0) {
+    return 0;
+  }
+  let init = [1];
+  if (num == 1) {
+    return 1;
+  }
+  for (var i = 0; i < num; i++) {
+    if (init.length == 1) {
+      init.push(1);
+      console.log(i, "first");
+      i++;
+    } else {
+      let curr = init[i - 1];
+      console.log(curr, i);
+      let prev = init[i - 2];
+      console.log(prev, i);
+      init.push(prev + curr);
+      console.log(init);
+    }
+  }
+  return init[num - 1];
 };
 
 // Do not edit below this line
