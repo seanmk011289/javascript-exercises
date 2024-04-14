ffc2616b8bfdbaed8da657c89f68180b1fa76958
Sean Kennedy <seanmk011289@gmail.com>
2024-04-14 09:39:29 -0500

    Added get titles code

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (arr) {
+  return arr.reduce((x, y) => {
+    return [...x, y.title];
+  }, []);
 };
 
 // Do not edit below this line
